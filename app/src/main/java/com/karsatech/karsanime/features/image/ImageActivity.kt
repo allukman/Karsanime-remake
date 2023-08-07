08a6019fa65a496072a8f831339a80bc1f8b5416
Al Lukman <allukman1802@gmail.com>
2023-08-07 10:34:42 +0700

    feature : add download image

--- app/src/main/java/com/karsatech/karsanime/features/image/ImageActivity.kt
+++ app/src/main/java/com/karsatech/karsanime/features/image/ImageActivity.kt
@@ -1,13 +1,36 @@
 package com.karsatech.karsanime.features.image
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.app.DownloadManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
+import com.karsatech.karsanime.R
 import com.karsatech.karsanime.databinding.ActivityImageBinding
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.net.URL
 
 class ImageActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityImageBinding
+    private var imgUrl: String? = null
+    private var msg: String? = ""
+    private var lastMsg = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,18 +41,157 @@ class ImageActivity : AppCompatActivity() {
     }
 
     private fun setupData() {
-        val data = intent.getStringExtra(DETAIL_IMAGE)
+        imgUrl = intent.getStringExtra(DETAIL_IMAGE)
+
         Glide.with(applicationContext)
-            .load(data)
+            .load(imgUrl)
             .fitCenter()
             .into(binding.ivDetailImage)
 
         binding.btnBack.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
+        binding.btnDownload.setOnClickListener {
+            doDownload(imgUrl.toString())
+        }
+    }
+
+    private fun doDownload(imageUrl: String) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            askPermissions()
+        } else {
+            downloadImage(imageUrl)
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    fun askPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            ) {
+                AlertDialog.Builder(this)
+                    .setTitle("Permission required")
+                    .setMessage("Permission required to save photos from the Web.")
+                    .setPositiveButton("Allow") { _, _ ->
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                            STORAGE_PERMISSION_CODE
+                        )
+                        finish()
+                    }
+                    .setNegativeButton("Deny") { dialog, _ -> dialog.cancel() }
+                    .show()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    STORAGE_PERMISSION_CODE
+                )
+            }
+        } else {
+            downloadImage(imgUrl.toString())
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            STORAGE_PERMISSION_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    downloadImage(imgUrl!!)
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Tidak bisa melakukan download karena tidak diberi izin",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return
+            }
+
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+
+    @SuppressLint("Range")
+    private fun downloadImage(url: String) {
+        val directory = File(Environment.DIRECTORY_PICTURES)
+
+        if (!directory.exists()) {
+            directory.mkdirs()
+        }
+
+        val downloadManager = this.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val downloadUri = Uri.parse(url)
+        val request = DownloadManager.Request(downloadUri).apply {
+            setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+                .setAllowedOverRoaming(false)
+                .setTitle(url.substring(url.lastIndexOf("/") + 1))
+                .setDescription("")
+                .setDestinationInExternalPublicDir(
+                    directory.toString(),
+                    url.substring(url.lastIndexOf("/") + 1)
+                )
+        }
+
+        val downloadId = downloadManager.enqueue(request)
+        val query = DownloadManager.Query().setFilterById(downloadId)
+        Thread(Runnable {
+            var downloading = true
+            while (downloading) {
+                val cursor: Cursor = downloadManager.query(query)
+                cursor.moveToFirst()
+                if (cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
+                    downloading = false
+                }
+                val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                msg = statusMessage(url, directory, status)
+                if (msg != lastMsg) {
+                    this.runOnUiThread {
+                        Toast.makeText(this, msg.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
+                    lastMsg = msg ?: ""
+                }
+                cursor.close()
+            }
+        }).start()
+    }
+
+    private fun statusMessage(url: String, directory: File, status: Int): String? {
+        var msg = ""
+        msg = when (status) {
+            DownloadManager.STATUS_FAILED -> getString(R.string.DOWNLOAD_STATUS_FAILED)
+            DownloadManager.STATUS_PAUSED -> getString(R.string.DOWNLOAD_STATUS_PAUSED)
+            DownloadManager.STATUS_PENDING -> getString(R.string.DOWNLOAD_STATUS_PENDING)
+            DownloadManager.STATUS_RUNNING -> getString(R.string.DOWNLOAD_STATUS_RUNNING)
+            DownloadManager.STATUS_SUCCESSFUL -> "Image downloaded successfully in $directory" + File.separator + url.substring(
+                url.lastIndexOf("/") + 1
+            )
+            else -> getString(R.string.DOWNLOAD_STATUS_ELSE)
+        }
+        return msg
     }
 
     companion object {
         const val DETAIL_IMAGE = "DETAIL_IMAGE"
+        private const val STORAGE_PERMISSION_CODE = 123
     }
 }
